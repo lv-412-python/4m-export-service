8ffcf3a3ab80fb281be11782c2e12297d836c44a
Anastasia Siromska <siromska99@gmail.com>
2019-07-19 13:21:52 +0300

    Added CORS.

--- export_service/__init__.py
+++ export_service/__init__.py
@@ -1,9 +1,11 @@
 """Init export service."""
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 
 APP = Flask(__name__)
+CORS(APP)
 API = Api(APP)
 
 # from export_service.views.export import export  #pylint: disable=wrong-import-position
